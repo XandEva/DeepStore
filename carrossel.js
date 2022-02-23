999c9ca2286d109872e4a689a14d627642ca1dbe
AlexandreEvangelista <alefernandocampos38@gmail.com>
2022-02-22 21:23:56 -0300

    carrosel.js

---
+++ carrossel.js
@@ -0,0 +1,15 @@
+// const imgs = document.getElementById("img");
+// const img = document.querySelectorAll("#img img")
+
+// let idx = 0;
+
+// function carrossel(){
+//     idx++;
+//     if(idx > img.length -1){ //quando o idx for maior que o tamanho(length) -1 a img vai voltar a ser 0 
+//         idx = 0
+//     }
+//     imgs.style.transform = `translateX(${-idx * 500}px)`;
+// }
+
+// setInterval(carrossel, 2000)
+
